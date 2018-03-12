c619e7e0d90b97b79d36a8fc354160a867a32d69
Allan Arnesen <allan.arnesen@gmail.com>
2018-03-12 12:13:17 +0100

    Moved the bot to RunBot file

---
+++ Chatbot/src/bots/RunBot.java
@@ -0,0 +1,51 @@
+package bots;
+
+import java.io.File;
+
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.History;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.utils.IOUtils;
+
+public class RunBot {
+
+	
+	private static final boolean TRACE_MODE = false;
+	static String botName = "super";
+	String resourcesPath = getResourcesPath();
+	Bot bot; 
+	Chat chatSession;
+	
+	public RunBot(){
+		if(bot == null){
+			
+			bot = new Bot("super", resourcesPath);;
+		}
+		
+		chatSession = new Chat(bot);
+		
+		bot.brain.nodeStats();
+		
+	}
+	public String talkToBot(String comment){
+		
+		String response = chatSession.multisentenceRespond(comment);
+		while (response.contains("&lt;"))
+			response = response.replace("&lt;", "<");
+		while (response.contains("&gt;"))
+			response = response.replace("&gt;", ">");
+		return response;
+		
+	}
+
+	private String getResourcesPath() {
+		File currDir = new File(".");
+		String path = currDir.getAbsolutePath();
+		path = path.substring(0, path.length() - 2);
+		System.out.println(path);
+		String resourcesPath = path + File.separator + "src" + File.separator;/*+ "main" + File.separator + "resources"*/
+		return resourcesPath;
+	}
+}
